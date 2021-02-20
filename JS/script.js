83813303b15b386dc4607fe1d2f6f4e7adaa8e04
SideRocketeer0 <daniel8thomasa@gmail.com>
2021-02-20 14:47:32 +0000

    added trigangle down and changed pic/color scheme

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,16 @@
 
 
+//add the v transistion from the first picture into the about me section
+//////////////////has a glitch effect at the bottom
+const right = document.getElementById('skewedRight');
+const left = document.getElementById('skewedLeft');
+window.addEventListener('scroll', function(){
+  const value1 = -15 + window.scrollY/45;
+  const value2 = 15 + window.scrollY/-45;
+  skewedRight.style.transform = "skewY(" + value1 + "deg)";
+  skewedLeft.style.transform = "skewY(" + value2 + "deg)";
+})
+
 const showRequiredCategory = event => {
   const getId = event.id;
   const links = document.querySelectorAll('.work-category button');
